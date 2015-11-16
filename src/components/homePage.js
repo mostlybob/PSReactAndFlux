c9d0859bf4d9bbc2b9b897f130d46a2e3ab1ea8a
Bob Campbell <mostlybob@gmail.com>
2015-11-15 21:51:11 -0600

    wired component up to home page

--- src/components/homePage.js
+++ src/components/homePage.js
@@ -4,7 +4,7 @@ var React = require('react');
 
 var Home = React.createClass({
 	render: function(){
-		return(
+		return (
 			<div className="jumbotron">
 				<h1>Pluralsight Administration</h1>
 				<p>React, React Router and Flux for ultra-responsive web apps.</p>
